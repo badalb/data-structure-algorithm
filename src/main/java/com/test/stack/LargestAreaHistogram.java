1cdcd5ef6ad4aa90d71c0a0bc0554c87b42f1c63
badalb <badal.cs@gmail.com>
2018-03-16 14:57:41 +0530

    stack algo added

---
+++ src/main/java/com/test/stack/LargestAreaHistogram.java
@@ -0,0 +1,40 @@
+package com.test.stack;
+
+public class LargestAreaHistogram {
+
+	public static void main(String[] args) {
+		int hist[] = { 6, 2, 5, 4, 5, 1, 6 };
+		System.out.println("Maximum area :" + calcMaxArea(hist, hist.length));
+	}
+
+	private static int calcMaxArea(int[] hist, int length) {
+		java.util.Stack<Integer> stack = new java.util.Stack<>();
+		int i = 0;
+		int top = 0;
+		int maxArea = 0;
+		while (i < hist.length) {
+			if (stack.isEmpty() || hist[stack.peek()] <= hist[i]) {
+				stack.push(i);
+				i++;
+			} else {
+				top = stack.peek();
+				stack.pop();
+				int currentMax = hist[top] * (stack.empty() ? i : i - stack.peek() - 1);
+				if (maxArea < currentMax) {
+					maxArea = currentMax;
+				}
+			}
+		}
+
+		while (stack.empty() == false) {
+			top = stack.peek();
+			stack.pop();
+			int currentMax = hist[top] * (stack.empty() ? i : i - stack.peek() - 1);
+
+			if (maxArea < currentMax) {
+				maxArea = currentMax;
+			}
+		}
+		return maxArea;
+	}
+}
