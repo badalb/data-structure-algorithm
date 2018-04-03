4ee556446a2e69308951ef23673aeb7acbea4796
badalb <badal.cs@gmail.com>
2018-04-03 09:34:12 +0530

    updated

--- src/main/java/com/test/dynamicprograming/MinCostToReachDest.java
+++ src/main/java/com/test/dynamicprograming/MinCostToReachDest.java
@@ -4,17 +4,19 @@ public class MinCostToReachDest {
 
 	static int INF = Integer.MAX_VALUE, N = 4;
 
-	static int minCost(int cost[][]) {
+	private static int minCost(int cost[][]) {
 		int dist[] = new int[N];
 		for (int i = 0; i < N; i++)
 			dist[i] = INF;
 		dist[0] = 0;
 
-		for (int i = 0; i < N; i++)
-			for (int j = i + 1; j < N; j++)
-				if (dist[j] > dist[i] + cost[i][j])
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (dist[j] > dist[i] + cost[i][j]) {
 					dist[j] = dist[i] + cost[i][j];
-
+				}
+			}
+		}
 		return dist[N - 1];
 	}
 
